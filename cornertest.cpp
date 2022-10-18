f0a3779c18620344046be1139da1469f7a23ee7d
fqy22 <fengqiyuan18@163.com>
2022-10-18 10:55:53 +0800

    trial succeed

--- cornertest.cpp
+++ cornertest.cpp
@@ -45,13 +45,50 @@ int main(){
         cout << "the current image is "<< i << "th" << endl;
         //将原图转换为对应的灰度图像。
         cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
-    }
-    //findCirclesGrid可以实现对于圆的圆心的提取,而findChessboardCorners可以用于棋盘角点的识别。
-    success= cv::findChessboardCorners(gray, cv::Size(CHECKERBOARD[0],CHECKERBOARD[1]),corner_pts,
-                 CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FAST_CHECK | CALIB_CB_NORMALIZE_IMAGE);
 
-    if(success){
+        //findCirclesGrid可以实现对于圆的圆心的提取,而findChessboardCorners可以用于棋盘角点的识别。
+        success= cv::findChessboardCorners(gray, cv::Size(CHECKERBOARD[0],CHECKERBOARD[1]),corner_pts,
+                        CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FAST_CHECK | CALIB_CB_NORMALIZE_IMAGE);
 
+        cout << success << endl;
+        //如果检测到了所有的棋盘上的角点，我们将细化像素坐标并将其显示在棋盘图像上
+        if(success){
+            cout << "success" << endl;
+            //这个类是用来定义迭代算法终止标准的类。
+            cv::TermCriteria criteria(TermCriteria::EPS | TermCriteria::Type::MAX_ITER, 30, 0.001);
+
+            //为给定的二维点细化像素坐标，不断的迭代，只到次数大于30或者是精读变化小于0.001
+            cv::cornerSubPix(gray, corner_pts, cv::Size(11,11),cv::Size(-1,-1),criteria);
+
+            //在棋盘上显示检测到的角点
+            cv::drawChessboardCorners(frame, cv::Size(CHECKERBOARD[0],CHECKERBOARD[1]), corner_pts, success);
+
+            // frame.show();
+            cv::imshow("名字",frame);
+            system("wait");
+            cv::waitKey(1);
+            //目前我对objp的理解不是很充分。
+            objpoints.push_back(objp);
+            imgpoints.push_back(corner_pts);
+        }
     }
+
+    cv::destroyAllWindows();
+
+    cv::Mat cameraMatrix, distCoeffs, R, T;
+    cv::calibrateCamera(objpoints, imgpoints, cv::Size(gray.rows,gray.cols), cameraMatrix, distCoeffs, R, T);
+
+    // //内参矩阵
+    std::cout << "cameraMatrix : " << cameraMatrix << std::endl;
+
+    // //透镜畸变畸变系数
+    std::cout << "distCoeffs : " << distCoeffs << std::endl;
+
+    // //R
+    std::cout << "Rotation vector : " << R << std::endl;
+
+    // //T
+    std::cout << "Translation vector : "<< T << std::endl;
+
     return 0;
 }
